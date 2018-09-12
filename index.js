719aed581c48eb8b4ac26bbe4322a4457206a3d9
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-12 05:07:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ function getRepositories() {
   req.send();
 }  
 
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
 
